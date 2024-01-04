b136f713940845633f41d219ff23ee9164c1d76d
Marcin1990x <marcin.konecki21@gmail.com>
2024-01-04 17:22:17 +0100

    First commit.

---
+++ src/main/java/pl/koneckimarcin/triathlontrainingmanagement/TriathlontrainingmanagementApplication.java
@@ -0,0 +1,13 @@
+package pl.koneckimarcin.triathlontrainingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriathlontrainingmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriathlontrainingmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/koneckimarcin/triathlontrainingmanagement/TriathlontrainingmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.koneckimarcin.triathlontrainingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriathlontrainingmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

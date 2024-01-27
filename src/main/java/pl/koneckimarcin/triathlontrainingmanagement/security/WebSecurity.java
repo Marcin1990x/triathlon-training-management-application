bce8ccb8bc925ee53d739b8c842ca4670d571c40
Marcin1990x <marcin.konecki21@gmail.com>
2024-01-27 17:33:23 +0100

    Added configuration for CSRF.

--- src/main/java/pl/koneckimarcin/triathlontrainingmanagement/security/WebSecurity.java
+++ src/main/java/pl/koneckimarcin/triathlontrainingmanagement/security/WebSecurity.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import pl.koneckimarcin.triathlontrainingmanagement.security.filter.CsrfCookieFilter;
 
 @Configuration
 public class WebSecurity {
@@ -17,12 +19,21 @@ public class WebSecurity {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+
         http
+//                .securityContext(context -> context.requireExplicitSave(false)) // for UI
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .authorizeHttpRequests(request ->
                         request
                                 .requestMatchers("/athletes/**").authenticated()
-                                .requestMatchers("/**").permitAll())
-                .csrf(AbstractHttpConfigurer::disable)
+                                .requestMatchers("/register", "/h2-console/**").permitAll())
+                .csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler)
+                        .ignoringRequestMatchers("/register", "/h2-console/**")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());

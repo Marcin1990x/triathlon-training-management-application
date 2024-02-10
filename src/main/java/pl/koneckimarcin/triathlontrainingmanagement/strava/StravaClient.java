9bc0efa918f830111eba8128dca997cf0c5bfc74
Marcin1990x <marcin.konecki21@gmail.com>
2024-02-10 19:18:58 +0100

    added method to get all activities after specified time.

--- src/main/java/pl/koneckimarcin/triathlontrainingmanagement/strava/StravaClient.java
+++ src/main/java/pl/koneckimarcin/triathlontrainingmanagement/strava/StravaClient.java
@@ -3,6 +3,9 @@ package pl.koneckimarcin.triathlontrainingmanagement.strava;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @Component
 public class StravaClient {
 
@@ -14,11 +17,29 @@ public class StravaClient {
         return callGetActivityById(activityId);
     }
 
+    public ActivityClientDto[] getAllActivities() {
+
+        return callGetAllActivities();
+    }
+
     private ActivityClientDto callGetActivityById(Long activityId) {
 
         String accessToken = StravaPropertyReader.getValue("strava_access_token");
 
         return rest.getForObject(STRAVA_URL + "activities/{activityId}/?access_token={accessToken}",
                 ActivityClientDto.class, activityId, accessToken);
+    }
+
+    private ActivityClientDto[] callGetAllActivities() {
+
+        //activities after
+        ZonedDateTime after = ZonedDateTime.of(
+                2024, 2, 1, 0, 0, 0, 0,
+                ZoneId.of("Europe/Warsaw"));
+
+        String accessToken = StravaPropertyReader.getValue("strava_access_token");
+
+        return rest.getForObject(STRAVA_URL + "athlete/activities?access_token={accessToken}&after={after}",
+                ActivityClientDto[].class, accessToken, after.toEpochSecond());
     }
 }

9ba7278d19b42533d8d2c782861ef82713c8cc01
Marcin1990x <marcin.konecki21@gmail.com>
2024-02-11 18:27:53 +0100

    Added simple service method to get, map and save activity from strava to db.

--- src/main/java/pl/koneckimarcin/triathlontrainingmanagement/strava/StravaController.java
+++ src/main/java/pl/koneckimarcin/triathlontrainingmanagement/strava/StravaController.java
@@ -3,6 +3,7 @@ package pl.koneckimarcin.triathlontrainingmanagement.strava;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,21 @@ public class StravaController {
     public void getActivityById(@RequestParam Long id) {
 
         System.out.println(stravaService.getActivityById(id));
+    }
+
+    @PreAuthorize("hasAnyAuthority('ATHLETE', 'COACH')")
+    @GetMapping("/strava/all")
+    public void getAllActivities() {
+
+        ActivityClientDto[] allActivities = stravaService.getAllActivities();
+        for(ActivityClientDto activity : allActivities){
+            System.out.println(activity);
+        }
+    }
+    @PreAuthorize("hasAnyAuthority('ATHLETE', 'COACH')")
+    @PostMapping("/strava/synchronize")
+    public void synchronizeActivitiesForAthlete() {
+
+        stravaService.synchronizeActivitiesForAthlete();
     }
 }
--- src/main/java/pl/koneckimarcin/triathlontrainingmanagement/strava/StravaService.java
+++ src/main/java/pl/koneckimarcin/triathlontrainingmanagement/strava/StravaService.java
@@ -2,6 +2,8 @@ package pl.koneckimarcin.triathlontrainingmanagement.strava;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.koneckimarcin.triathlontrainingmanagement.training.trainingRealizationStrava.TrainingRealizationStravaEntity;
+import pl.koneckimarcin.triathlontrainingmanagement.training.trainingRealizationStrava.TrainingRealizationStravaRepository;
 
 @Service
 public class StravaService {
@@ -9,8 +11,33 @@ public class StravaService {
     @Autowired
     private StravaClient stravaClient;
 
-    public ActivityClientDto getActivityById(Long id){
+    @Autowired
+    private TrainingRealizationStravaRepository realizationStravaRepository;
+
+    public ActivityClientDto getActivityById(Long id) {
 
         return stravaClient.getActivityByIdFromClient(id);
+    }
+
+    public ActivityClientDto[] getAllActivities() {
+
+        return stravaClient.getAllActivities();
+    }
+
+    public void synchronizeActivitiesForAthlete() {
+
+        //retrieve existing strava activities id's for loggedIn athlete by it's strava id
+        //if new add to db
+        ActivityClientDto activity = stravaClient.getActivityByIdFromClient(10732991983L);// for test hardcoded stravaId
+        mapToTrainingRealizationStravaAndSaveToDb(activity);
+    }
+
+    private void mapToTrainingRealizationStravaAndSaveToDb(ActivityClientDto activity) {
+
+        //don't forget to set athleteId
+        TrainingRealizationStravaEntity trainingRealizationStrava = activity.mapToRealizationStravaEntity();
+        trainingRealizationStrava.setAthleteId(2L); // for test
+
+        realizationStravaRepository.save(trainingRealizationStrava);
     }
 }
